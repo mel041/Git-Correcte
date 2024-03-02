c325db8807b7394e1584e9e0d6712dd00cbed26a
Mel <melrosalinda04@gmail.com>
2024-03-02 19:21:39 +0100

    segon mel

--- code_1.py
+++ code_1.py
@@ -11,4 +11,15 @@ def main():
         print("4. Exit") 
         
         opcion = input("Select an option (1/2/3/4): ") 
-        
+        if opcion == "1":   
+            agregar_contacto(agenda)
+
+        elif opcion == "2": 
+            ver_contactos(agenda)
+        elif opcion == "3": 
+            buscar_contacto(agenda)
+        elif opcion == "4":
+            print("¡See you soon!") 
+            break 
+        else:   
+            print("Invalid option. Please select a valid option.") 
