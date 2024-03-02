a3d2fcd3686f65031dbd04e6aaa99350402782c4
Mel <melrosalinda04@gmail.com>
2024-03-02 19:19:53 +0100

    first mel

---
+++ code_1.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome to the phone book!!")
+
+    agenda = {}
+
+    while True: 
+
+        print("1. Add contact") 
+        print("2. View contacts") 
+        print("3. Search contact") 
+        print("4. Exit") 
+        
+        opcion = input("Select an option (1/2/3/4): ") 
+        
