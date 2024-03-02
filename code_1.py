33264c73d1ec2548a5e74aa1d221fb61330fe1ea
Souhaila <souhailairzi@gmail.com>
2024-03-02 19:23:58 +0100

    segon 2

--- code_1.py
+++ code_1.py
@@ -7,3 +7,16 @@ def agregar_contacto(agenda):
     agenda[nombre] = telefono
     # Print a success message
     print("Contact added successfully.")
+
+def ver_contactos(agenda):
+    # Check if the phonebook is empty
+    if len(agenda) == 0:
+        print("The phonebook is empty.")
+    else:
+        # Print the list of contacts
+        print("Contact list:")
+        # Loop through the items in the phonebook and print each contact's name and phone number
+        for nombre, telefono in agenda.items():
+              # For each tuple pair, print the contact's name and phone number. 
+              # 'nombre' is the variable representing the contact's name and 'telefono' is the variable representing the contact's phone number.
+            print("Name:", nombre, "\tPhone number:", telefono)
